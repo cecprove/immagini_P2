176e67c8ef658be151a8b9b10c80466175b90d37
CamiKiwi <camilla.mussoni@gmail.com>
2019-02-15 13:59:37 +0100

    Update progetto_p2.m

--- progetto_p2.m
+++ progetto_p2.m
@@ -1,22 +1,18 @@
 %% P2
-% crea database
+% crea database:
+% dalla directory/path personale andiamo a prendere il database Yale fornito
 imagespath=dir('C:\Users\cecil\Documents\Universita\ElaborazioneImmagini\Progetto\Database_per_gli_elaborati_di_Tipo_2_20190202\Yale\Yale');
 imagespath;
-ImagesPath=imagespath(4:end,:);% Per eliminare i primi tre elementi che non sono immagini
-images=struct;
+ImagesPath=imagespath(4:end,:);% Sono eliminati i primi tre elementi perché non sono immagini
 
+% creiamo una struct images in cui inserire solo le immagini(unit8) e le tipologie(char)/classi
+images=struct;
 for i=1:size(ImagesPath,1)
     images(i).images=imread([ImagesPath(i).folder,'/',ImagesPath(i).name]);
-    % creo il path totale dell'immagine, quindi
-    % sto caricando con ogni ciclo la struttura ad ogni immagine
-    % Se volessi solo l'immagine 7 scriverò images(7).images e gli dico di
-    % andare a prendere la settima immagine che ha  caricato nel database, in
-    % questo caso la struct
-    %creo struct con tuttii nomi delle immagini
     images(i).tipologia = extractAfter(ImagesPath(i).name,".");
 end
 
-%% Associamo ad ogni etichetta un numero
+%% È associata un'etichetta numerica(double) ad ogni tipologia(char) 
 for i=1:size(images,2)
     switch images(i).tipologia
         case 'glasses'
@@ -47,17 +43,18 @@ for i=1:size(images,2)
 end
 
 %% creiamo le HOG feature di tutte le immagini
-% creo la struttura database.condizione(a).feature(b).matrice.matrice 
-% a= 1--> senza rumore e a=2,3,4,5 rumore crescente
-% b= 1,2 e 3 son le varie CellSize
 
+% Inseriamo le feature estratte nella struttura: database.condizione(a).feature(b).matrice.matrice 
+% a= 1--> senza rumore e a=2,3,4,5 con rumore crescente addizionato all'immagine originale
+% b= 1,2 e 3 sono le varie CellSize, rispettivamente [2,2] [4,4] [8,8]
 
-% dobbiamo far variare con un ciclo for a da 1 a 5 per i vari livelli di
-% rumore
-c=[2 4 8];
-rumore = logspace(-5,-1,5);
+% dobbiamo far variare con un ciclo "for" a da 1 a 5 per i vari livelli di rumore
+% e un ciclo "for" b da 1 a 3 per le diverse CellSize.
+
+c=[2 4 8]; %vettore che rappresenta le dimensioni CellSize di nostro interesse, varia con b
+rumore = logspace(-5,-1,5); %funzione per definire il rumore
 for a=1:5 
-   for b=1:3
+   for b=1:3 
           for i=1:size(images,2)
            [database.condizione(a).feature(b).matrice(i).matrice,visualization] = extractHOGFeatures((double(images(i).images)+sqrt(rumore(a))*randn(size(images(i).images))), ...
                'CellSize',[c(b) c(b)]);
@@ -66,31 +63,36 @@ for a=1:5
    end
 end
 
-%prova
+%prova per visulaizzare le HOG 
 %imshow(database.dimensione.dominio(1).immagini(2).matrice(11).matrice);
 %hold on
 %plot(visualization)
 
-%% Logica leave one out
-v=1:size(images,2);
+%% Classificatore con logica leave one out
+
+v=1:size(images,2); %vettore degli indici delle immagini totali
 for a=1:5
     for b=1:3 
-        for i=1:size(images,2) %i è gli indici di test. La feature testata è l'i-esima
+        for i=1:size(images,2) % "i" indica la feature di test. (La feature testata è l'i-esima)
        
-            t= setdiff(v,i); % vettore indici training, tutte le features tranne quella di test i-ma
+            t= setdiff(v,i); % vettore degli indici del training (tutte le features esclusa quella di test i-ma)
         
+        % vettori contententi le features e le etichette per il training da passare al fitecoc
             for k=1:size(t,2)%163
                training_features(k,:) = database.condizione(a).feature(b).matrice(t(k)).matrice;
                training_labels(k) =images(t(k)).etichetta;
             end
        
-           test_feature = database.condizione(a).feature(b).matrice(i).matrice; %per il classifier     
-           risultati.condizioni(a).cellsize(b).test(i).verita = images(i).etichetta; %ground truth per confronto
+       % vettore con la feature da testare con il classificatore
+           test_feature = database.condizione(a).feature(b).matrice(i).matrice; 
+       % stuct contenente la ground truth per confronto con predizione e per calcolare la matrice di confusione
+           risultati.condizioni(a).cellsize(b).test(i).verita = images(i).etichetta; 
        
-           %classifier
+           %classifier fitcecoc in ingresso: feature e etichette per il training, logica "one versus all"
            classifier= fitcecoc(training_features, training_labels, 'Coding', 'onevsall');      
+           % etichetta predetta dal classificatore per la feature testata
            risultati.condizioni(a).cellsize(b).test(i).predizioni= predict(classifier, test_feature);
-        i
+        i % stampiamo l'indice per sapere a che ciclo è arrivato il programma
          end
     end
 end
@@ -99,17 +101,22 @@ end
 %% Calcolo della matrice di confusione
 for a=1:5
     for b=1:3
-        % il problema è come passiamo i dati alla confusion mat poichè non
-        % prende in ingresso una struct ma un vettore. 
+       
+        % la funzione confusionmat accetta in ingresso due vettori:
+        % creiamo due vettori con i dati contenuti nella struct "risultati" per verità e predizione
         for i=1:size(risultati.condizioni(a).cellsize(b).test,2)
            verita(i,1) = risultati.condizioni(a).cellsize(b).test(i).verita;
            predizione(i,1) = risultati.condizioni(a).cellsize(b).test(i).predizioni;
         end
-             
+        % calcoliamo la matrice di confusione con "confusionmat"
         analisi_dati.condizioni(a).cellsize(b).matrix_confusione = confusionmat(verita,...
             predizione);
         
+        % creiamo una struct con le analisi dei risultati: accuracy, recall, precision
+        
+        % calcolo accuracy
         analisi_dati.condizioni(a).cellsize(b).accuracy = trace(analisi_dati.condizioni(a).cellsize(b).matrix_confusione)/sum(sum(analisi_dati.condizioni(a).cellsize(b).matrix_confusione));
+        
         d = diag(analisi_dati.condizioni(a).cellsize(b).matrix_confusione);
         for i=1:size(analisi_dati.condizioni(a).cellsize(b).matrix_confusione,1)
             %calcolo recall
