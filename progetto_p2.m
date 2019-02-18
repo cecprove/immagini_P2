d8d8f3fac20c8095c79d7d52d53265f975210c00
cecprove <47523654+cecprove@users.noreply.github.com>
2019-02-18 17:38:22 +0100

    Add files via upload

--- progetto_p2.m
+++ progetto_p2.m
@@ -52,11 +52,11 @@ end
 % e un ciclo "for" b da 1 a 3 per le diverse CellSize.
 
 c=[2 4 8]; %vettore che rappresenta le dimensioni CellSize di nostro interesse, varia con b
-rumore = logspace(-5,-1,5); %funzione per definire il rumore (double), anche l'immagine va trasformata in double. 
-for a=1:5 
-   for b=1:3 
-          for i=1:size(images,2)
-           [database.condizione(a).feature(b).matrice(i).matrice,visualization] = extractHOGFeatures((double(images(i).images)+sqrt(rumore(a))*randn(size(images(i).images))), ...
+rumore = logspace(-5,-1,5); %funzione per definire il rumore
+for a=1:5 % scorro rumori
+   for b=1:3 % scorro le cell size
+          for i=1:size(images,2) %164 immagini
+           [database.condizione(a).feature(b).matrice(i).matrice,visualization] = extractHOGFeatures((im2double(images(i).images)+sqrt(rumore(a))*randn(size(images(i).images))), ...
                'CellSize',[c(b) c(b)]);
           end          
     
@@ -69,47 +69,51 @@ end
 %plot(visualization)
 
 %% Classificatore con logica leave one out
-
+% da fare rumore 0.0010 per a = 3 con cellsize 2 e 4 (b = 1 e 2)(circa 12 ore)
 v=1:size(images,2); %vettore degli indici delle immagini totali
-a=1:5 %training del classificatore con tutte le immagini (anche rumorose)
-    for b=1:3 
-    clear training_features;
-    clear training_labels;
-    clear test_feature;
+for a =1:5
+    for b=1:3 %cell size
+        clear training_features;
+        clear training_labels;
+        clear test_feature;
         for i=1:size(images,2) % "i" indica la feature di test. (La feature testata è l'i-esima)
        
             t= setdiff(v,i); % vettore degli indici del training (tutte le features esclusa quella di test i-ma)
         
         % vettori contententi le features e le etichette per il training da passare al fitcecoc
-            for k=1:size(t,2)%163
+            for k=1:size(t,2)%163 vettore senza indice di training
                training_features(k,:) = database.condizione(a).feature(b).matrice(t(k)).matrice;
                training_labels(k) =images(t(k)).etichetta;
             end
        
-       % vettore con la feature da testare con il classificatore
-           test_feature = database.condizione(a).feature(b).matrice(i).matrice; 
-       % stuct contenente la ground truth per confronto con predizione e per calcolare la matrice di confusione
-           risultati.condizioni(a).cellsize(b).test(i).verita = images(i).etichetta; 
-       
+      
            %classifier fitcecoc in ingresso: feature e etichette per il training, logica "one versus all"
-           classifier= fitcecoc(training_features, training_labels, 'Coding', 'onevsall');      
-           % etichetta predetta dal classificatore per la feature testata
-           risultati.condizioni(a).cellsize(b).test(i).predizioni= predict(classifier, test_feature);
-           end
+           classifier= fitcecoc(training_features, training_labels, 'Coding', 'onevsall');
+           
+            % vettore con la feature da testare con il classificatore
+             test_feature = database.condizione(a).feature(b).matrice(i).matrice; 
+             
+              % stuct contenente la ground truth per confronto con predizione e per calcolare la matrice di confusione
+             risultati.condizioni(a).cellsize(b).test(i).verita = images(i).etichetta; 
+           
+            % etichetta predetta dal classificatore per la feature testata.,
+             risultati.condizioni(a).cellsize(b).test(i).predizioni= predict(classifier, test_feature);
+          
         i % stampiamo l'indice per sapere a che ciclo è arrivato il programma
-         end
-  end
-end
+        end
+    end
+ end
+
 
 
 %% Calcolo della matrice di confusione
 
-for a=1:5
-    for b=1:3
+for a=1:5 %rumori
+    for b=1:3 % cell size
        
         % la funzione confusionmat accetta in ingresso due vettori:
         % creiamo due vettori con i dati contenuti nella struct "risultati" per verità e predizione
-        for i=1:size(risultati.condizioni(a).cellsize(b).test,2)
+        for i=1:size(risultati.condizioni(a).cellsize(b).test,2) %fino a 164 
            verita(i,1) = risultati.condizioni(a).cellsize(b).test(i).verita;
            predizione(i,1) = risultati.condizioni(a).cellsize(b).test(i).predizioni;
         end
@@ -133,20 +137,3 @@ for a=1:5
         end   
     end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
